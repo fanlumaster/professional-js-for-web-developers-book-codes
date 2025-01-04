a29b06573d34f6af8f342f9fcc91781419e48333
fanlumaster <1730976608@qq.com>
2025-01-05 06:23:36 +0800

    init commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import appHtmlContent from "./html/app.html?raw";
+
+document.querySelector("#app").innerHTML = appHtmlContent;
+
+console.log(
+  "Here is all my codes for professional javascript for web developers book."
+);
